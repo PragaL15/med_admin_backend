2b5526cbb926d1d36469f44e6f69ef9bccf2ad38
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2024-11-03 20:49:43 +0530

    Updated new version

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/PragaL15/med_admin_backend/database"
-	"github.com/gofiber/fiber/v2"
+	routers "github.com/PragaL15/med_admin_backend/src/routers/user"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
-	app := fiber.New()
 	database.InitializeDB()
+	defer database.DB.Close()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	// Initialize router
+	router := routers.SetupRoutes()
 
-	app.Listen(":3000")
+	// CORS settings
+	corsOrigin := handlers.AllowedOrigins([]string{"http://localhost:5173"}) // Allow your frontend origin
+	corsMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
+	corsHeaders := handlers.AllowedHeaders([]string{"Origin", "Content-Type", "Accept"})
+
+	// Start the server with CORS enabled
+	log.Println("Server starting on :8080")
+	if err := http.ListenAndServe(":8080", handlers.CORS(corsOrigin, corsMethods, corsHeaders)(router)); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
